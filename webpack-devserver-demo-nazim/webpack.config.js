5af19565daadeaa3637ce433ba1bad6dd07eea24
Nazim khan <Najimulla.khan@sooryen.com>
2019-05-16 15:52:56 +0530

    provided changes to generate chunk files

--- webpack-devserver-demo-nazim/webpack.config.js
+++ webpack-devserver-demo-nazim/webpack.config.js
@@ -1,47 +1,30 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var package     = require('../package.json');
-var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); 
 var path = require("path");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
-    entry: {
-        vendor: Object.keys(package.dependencies),
-        app: "./src/scripts/app.js",
-        settings: "./src/scripts/settings.js"
+    entry: { 
+        app: ["./src/scripts/app.js","./src/scripts/settings.js"]
     }, 
     output: {
-        path: path.join(__dirname, "../dist/"),
+        path: path.join(__dirname, "dist"),
         filename: "[name].bundle.js",
     },
     watch:true,
     resolve: { extensions: [".js", ".ts"] },
     devServer: {
         contentBase: path.join(__dirname, "../dist/"),
-        port: 9000
+        port: 1234,
+        compress: true,
+        writeToDisk: true,
     },
-    plugins: [
-        new CommonsChunkPlugin({
-            name: 'shared',
-            minChunks: 2
-        }),
+    plugins: [ 
         new HtmlWebpackPlugin({
-            hash: true,
-            title: 'My Awesome application',
-            myPageHeader: 'Hello World',
-            template: './src/index.html',
-            chunks: ['vendor', 'shared', 'app'],
-            path: path.join(__dirname, "../dist/"),
-            filename: 'index.html' 
+            template: 'src/index.html',
+            filename: 'index.html',
         }),
-        new HtmlWebpackPlugin({
-            hash: true,
-            title: 'My Awesome application',
-            myPageHeader: 'Settings',
-            template: './src/index.html',
-            chunks: ['vendor', 'shared', 'settings'],
-            path: path.join(__dirname, "../dist/"),
-            filename: 'settings.html'
-        })
+        new CleanWebpackPlugin(['dist']) 
    ]
 
 }
