b7795f6a7425b4a8c92b36817060183640c66b89
Parth Modi <parth.modi@sooryen.com>
2019-05-30 10:19:05 +0530

    removed commented code

--- plugins-example/src/index.js
+++ plugins-example/src/index.js
@@ -3,7 +3,6 @@ function component() {
     const element = document.createElement('h1');
     element.innerHTML = "Webpack Plugins";
     return element;
-    //a asdasd.asdasdsdfasdxvfg
   }
   
   document.body.appendChild(component());
